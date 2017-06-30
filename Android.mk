16192207212b7dfd65eca5ec5428c5bfeb983ef3
GuaiYiHu <guaiyihu@foxmail.com>
2017-06-30 21:58:55 +0800

    build for x80plus_h5c5

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter chuwi_vi10plus cube_iwork8ultimate,$(TARGET_DEVICE)),)
+ifneq ($(filter chuwi_vi10plus cube_iwork8ultimate x80plus_h5c5,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 # Hack for prebuilt kernel
